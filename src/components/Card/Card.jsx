0efc96bfb935d5ebbda438fc7873b7ce1d2ca766
Julianne Adams <julianne@julianneadams.info>
2023-09-05 07:04:26 -0400

    Correct webfont loading and image sizes for performance improvement

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,7 +1,12 @@
 const Card = ({ id, name, email }) => {
   return (
     <div className="tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
-      <img src={`https://robohash.org/${id}?200x200`} alt="card" />
+      <img
+        src={`https://robohash.org/${id}?size=200x200`}
+        alt="card"
+        width={200}
+        height={200}
+      />
       <div>
         <h2>{name}</h2>
         <p>{email}</p>
